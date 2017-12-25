4e3fe76d7cd5ffe0f2faa37b193f2555a0bdc96e
weihua yuan <itheima_test@sina.com>
2017-12-25 17:07:48 +0800

    向本地仓库提供project

---
+++ ssm/src/cn/itcast/core/pojo/User.java
@@ -0,0 +1,32 @@
+package cn.itcast.core.pojo;
+
+import java.util.Date;
+
+public class User {
+
+	private Integer id;
+	private String name;
+	private Date birthday;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+}
